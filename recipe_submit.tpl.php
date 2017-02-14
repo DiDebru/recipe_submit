a847162cb159a3721e965deea22703e8b6f6d768
Insasse <schmidt.dirk33@gmail.com>
2017-02-14 09:21:51 +0100

    Prototype

---
+++ recipe_submit.tpl.php
@@ -0,0 +1,9 @@
+<em class='recipe_submit_title'><?php print $results['recipe_title'] ?></em>
+<ul class="recipe_submit_details">
+  <li class="recipe_submit_description"><?php print $results['recipe_description'] ?></li>
+  <li class="recipe_submit_ingredients"><?php print $results['recipe_ingredients'] ?></li>
+  <li class="recipe_submit_instructions"><?php print $results['recipe_instructions'] ?></li>
+</ul>
+<em class='recipe_submit_author_name'><?php print t('Author : ')?><?php print $results['author_name'] ?></em><br/>
+<em class='recipe_submit_author_email'><?php print t('Author email : ')?><?php print $results['author_email'] ?></em><br/>
+<em class="recipe_submit_submitted"><?php print format_date($results['submitted'],'long'); ?></em>
