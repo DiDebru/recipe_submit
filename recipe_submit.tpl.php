1f4807e10ed458822d39bdeaf0c4589f9b8a61cb
Insasse <schmidt.dirk33@gmail.com>
2017-02-14 12:55:16 +0100

    Code style and cleanup.

--- recipe_submit.tpl.php
+++ recipe_submit.tpl.php
@@ -1,4 +1,8 @@
-<em class='recipe_submit_title'><?php print $results['recipe_title'] ?></em>
+<?php if (empty($results)): ?>
+  <a href="/recipe/add"><?php t('Click here to add a recipe!')?></a>
+<?php endif; ?>
+<?php if ($results): ?>
+<h2 class='recipe_submit_title'><?php print $results['recipe_title'] ?></h2>
 <ul class="recipe_submit_details">
   <li class="recipe_submit_description"><?php print $results['recipe_description'] ?></li>
   <li class="recipe_submit_ingredients"><?php print $results['recipe_ingredients'] ?></li>
@@ -7,3 +11,5 @@
 <em class='recipe_submit_author_name'><?php print t('Author : ')?><?php print $results['author_name'] ?></em><br/>
 <em class='recipe_submit_author_email'><?php print t('Author email : ')?><?php print $results['author_email'] ?></em><br/>
 <em class="recipe_submit_submitted"><?php print format_date($results['submitted'],'long'); ?></em>
+<?php endif; ?>
+
